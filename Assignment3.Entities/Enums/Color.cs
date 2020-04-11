cf594dbd317a0a01e2782401b6c1fad3c299a21f
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-04-11 17:09:17 +0100

    Add Enums and TSirt.cs

---
+++ Assignment3.Entities/Enums/Color.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Entities.Enums
+{
+    enum Color
+    {
+        Blue,
+        Green,
+        Indigo,
+        Orange,
+        Red,
+        Violet,
+        Yellow
+    }
+}
---
+++ Assignment3.Entities/TShirt.cs
@@ -0,0 +1,18 @@
+﻿using Assignment3.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Entities
+{
+    class TShirt
+    {
+        //============================= Properties =====================================
+        public Fabric Fabric { get; }
+        public Color Color { get; }
+        public Size Size { get; }
+        public decimal Price { get; set; }
+    }
+}
